759ec17bbf3d6a8ff082eeed6b311add6ce38cac
Brian-ED <brianellingsgaard9@gmail.com>
2024-02-28 20:36:45 +0000

    trying stuff

--- html/bank.js
+++ html/bank.js
@@ -3,7 +3,16 @@ function getCurrentMoney(){
     method: "GET",
     mode: "cors",
     headers: {
-      "Content-type": "application/json"
+      host: '192.168.1.57:3000',
+      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0',
+      accept: '*/*',
+      'accept-language': 'en-GB,en;q=0.5',
+      'accept-encoding': 'gzip, deflate',
+      referer: 'http://brian-e.online/',
+      'content-type': 'application/json',
+      origin: 'http://brian-e.online',
+      dnt: '1',
+      connection: 'keep-alive'
     }
   })
   .then((response) => response.json())
@@ -14,6 +23,8 @@ function getCurrentMoney(){
     setText()
   });
 }
+
+
 
 function setText() {
   let dispUserMoney    = Math.round(userMoney*100)/100
