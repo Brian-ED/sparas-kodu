d5ed7b91add4d14298ecd22b5034c0e6347518b8
Brian-ED <brianellingsgaard9@gmail.com>
2024-02-29 10:04:07 +0000

    attempt at automating starting server

--- move.sh
+++ move.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-rm -rf /var/www/html
-cp -a html /var/www/
+echo "$(dirname "$0")/server"
+cd "$(dirname "$0")/server"
+sudo cp -rf ../html /var/www/
+node echo.js 1>/dev/null 2>/dev/null & disown > processLoc.txt
