0258126935ccd84714e1ddb8e7a83c6639d1c232
Brian-ED <brianellingsgaard9@gmail.com>
2024-02-29 10:11:08 +0000

    removed process text file

--- move.sh
+++ move.sh
@@ -2,4 +2,5 @@
 echo "$(dirname "$0")/server"
 cd "$(dirname "$0")/server"
 sudo cp -rf ../html /var/www/
-node echo.js 1>/dev/null 2>/dev/null & disown > processLoc.txt
+node echo.js 1>/dev/null 2>/dev/null & disown
+ps
